7ed8c0f42749079b1365a6ff256dcbaed7f95e4f
Joe Sergeant <me@joeserg.com>
2012-11-07 22:14:23 +0000

    Added location field to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121106083709) do
+ActiveRecord::Schema.define(:version => 20121107215109) do
 
   create_table "statuses", :force => true do |t|
     t.text     "content"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(:version => 20121106083709) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "location"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
