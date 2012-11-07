470edc5106c67d39ebdefbc23c34095c75a63e96
Joe Sergeant <me@joeserg.com>
2012-11-07 08:51:39 +0000

    Changed sorting so newest statuses show at top

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -5,7 +5,7 @@ class StatusesController < ApplicationController
   # GET /statuses
   # GET /statuses.json
   def index
-    @statuses = Status.all
+    @statuses = Status.all(:order => 'created_at DESC')
 
     respond_to do |format|
       format.html # index.html.erb
